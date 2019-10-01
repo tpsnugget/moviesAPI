b7d1aed981fccbfc117f37079f03e0c10acbfed6
Michael Giebner <mikegiebner@gmail.com>
2019-09-30 20:24:11 -0400

    # Version 2: Colt Steel, Web Dev Bootcamp, Section 27, Chapter 286 # Movie API App: Results Route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var request = require('request')
 
 app.set('view engine', 'ejs')
 
@@ -9,10 +10,25 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
    res.render('home')
 })
+
+app.get('/results', (req, res) => {
+   request('http://www.omdbapi.com/?apikey=thewdb&s=Bloodsport', (error, response, body) => {
+      
+      if (!error && response.statusCode == 200) {
+         var data = JSON.parse(body)
+         // eval(require('locus'))
+         for (var i = 0; i < data.Search.length; i++) {
+            // eval(require('locus'))
+            console.log(data.Search[i].Title)
+         }         
+      }
+   })
+   res.render('home')
+})
 //==============
 //    Routes
 //==============
 
-app.listen(3000, (req, res) => {
+app.listen(3000, process.env.IP, (req, res) => {
    console.log('The server has started...')
 })
